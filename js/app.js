4622ae541a8cc2ee6a573eb668af6de57975fe6d
Nick Senger <nsenger@mail.sfsu.edu>
2016-01-20 00:03:36 -0800

    added chart

--- js/app.js
+++ js/app.js
@@ -3,63 +3,62 @@ var details = {
   chosen: [],
   items: [],
   rowInfo: [],
+  fresh: true,
   open: false,
   headers: ['Item Name', 'Clicks', '# of Renders', 'Click Ratio'],
   div: document.getElementById('detContainer'),
   infoDiv: document.getElementById('detailsInfo'),
+  chartEl: document.getElementById('myChart'),
   detailsButton: document.createElement('h2')
 };
 details.showDetails = function(){
-  var objRef = this;
   this.detailsButton.textContent = 'Show Details';
   this.detailsButton.addEventListener('click', function(){
-    if (objRef.open === false){
-      objRef.render();
+    if (details.open === false){
+      details.render();
+      details.refreshButton();
     } else {
-      objRef.detailsButton.textContent = 'Show Details';
-      objRef.clear();
+      details.detailsButton.textContent = 'Show Details';
+      details.open = false;
+      details.div.removeChild(details.buttonEl);
+      details.detChart.destroy();
     }
   })
   this.div.appendChild(this.detailsButton);
 };
+details.refreshButton = function(){
+  details.buttonEl = document.createElement('img');
+  details.buttonEl.setAttribute('src', 'images/refresh.jpg');
+  details.buttonEl.className = 'refreshImg';
+  details.buttonEl.addEventListener('click', function(){
+    details.render();
+  })
+  details.buttonEl.addEventListener('mouseover', function(){
+    details.buttonEl.setAttribute('src', 'images/refreshO.jpg');
+  })
+  details.buttonEl.addEventListener('mouseout', function(){
+    details.buttonEl.setAttribute('src', 'images/refresh.jpg');
+  })
+  details.div.appendChild(details.buttonEl);
+}
 details.render = function(){
   this.detailsButton.textContent = 'Hide Details'
   this.open = true;
-  this.details = document.createElement('table');
-  this.details.className = 'detTable';
-  this.infoDiv.appendChild(this.details);
-  var headrow = document.createElement('tr');
-  this.details.appendChild(headrow);
-  for (var i = 0; i < this.headers.length; i++){
-    column = document.createElement('td');
-    column.className = 'detHead';
-    column.textContent = this.headers[i];
-    headrow.appendChild(column);
+  chartData.labels = [];
+  chartData.datasets[0].data = [];
+  chartData.datasets[1].data = [];
+  this.items.forEach(function(product){
+    chartData.labels.push(product.itemName);
+    chartData.datasets[0].data.push(product.clickCount);
+    chartData.datasets[1].data.push(product.renderCount);
+  }, this);
+  this.details = document.getElementById('myChart').getContext("2d");
+  if(this.fresh === false){
+    console.log('refreshing chart..')
+    this.detChart.destroy();
   }
-  for (var i = 0; i < this.items.length; i++){
-    var row = document.createElement('tr');
-    this.details.appendChild(row);
-    this.rowInfo[0] = this.items[i].itemName;
-    this.rowInfo[1] = this.items[i].clickCount;
-    this.rowInfo[2] = this.items[i].renderCount;
-    var percentClick = parseFloat(Math.round((this.items[i].clickCount / this.items[i].renderCount) * 100));
-    if (isNaN(percentClick)){
-      this.rowInfo[3] = 'N/A';
-    } else {
-      this.rowInfo[3] = percentClick + '%';
-    }
-    for (var j = 0; j < this.rowInfo.length; j++){
-      var cell = document.createElement('td');
-      cell.className = 'detCell';
-      cell.textContent = this.rowInfo[j];
-      row.appendChild(cell);
-    }
-  }
-};
-details.clear = function(){
-  this.detailsButton.textContent = 'Show Details';
-  this.open = false;
-  this.infoDiv.removeChild(this.details);
+  this.detChart = new Chart(this.details).Bar(chartData, {responsive: true});
+  this.fresh = false;
 };
 
 function item(indexNum, name, filepath){
@@ -69,7 +68,6 @@ function item(indexNum, name, filepath){
   this.renderCount = 0;
   this.clickCount = 0;
 }
-
 item.prototype.render = function() {
   var objRef = this;
   var container = document.getElementById('container');
@@ -92,10 +90,10 @@ item.prototype.render = function() {
   this.img.className = 'itemImg';
 
   this.img.addEventListener('mouseover', function(){
-    objRef.superSizeMe(260, 320, 4, 8, 0.6, 1);
+    objRef.superSizeMe(260, 280, 2, 8, 0.6, 1);
   })
   this.img.addEventListener('mouseout', function(){
-    objRef.superSizeMe(320, 260, -4, 8, 1, 0.6);
+    objRef.superSizeMe(280, 260, -2, 8, 1, 0.6);
   })
 
   this.divEl.appendChild(this.img);
@@ -129,6 +127,27 @@ item.prototype.resize = function(size, objRef, delay, opacity){
   }, delay);
 }
 
+var chartData = {
+  labels: [],
+  datasets: [
+    {
+      label: "Number of Clicks",
+      data: [],
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+    },
+    {
+      label: "Click/Render Ratio",
+      data: [],
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+    }
+  ]
+};
 
 function randNums(){ // Returns an array of 3 unique random numbers between 0 and 14
   var array = [0, 0, 0];
@@ -153,10 +172,6 @@ function refresh() {
   container.removeChild(details.items[details.chosen[1]].divEl);
   container.removeChild(details.items[details.chosen[2]].divEl);
   printItems();
-  if (details.open === true){
-    details.clear();
-    details.render();
-  }
 }
 
 details.items[0] = new item(0, 'R2D2 Bag', 'images/bag.jpg');
