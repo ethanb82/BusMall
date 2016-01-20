6dc01f4a9574d06d17673a0af4d511d6c193c938
Nick Senger <nsenger@mail.sfsu.edu>
2016-01-20 10:59:53 -0800

    dried out code a bit

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ var details = {
   numChosen: 0,
   chosen: [],
   items: [],
-  rowInfo: [],
   fresh: true,
   open: false,
-  headers: ['Item Name', 'Clicks', '# of Renders', 'Click Ratio'],
   div: document.getElementById('detContainer'),
   infoDiv: document.getElementById('detailsInfo'),
   chartEl: document.getElementById('myChart'),
@@ -14,7 +12,7 @@ var details = {
 details.showDetails = function(){
   this.detailsButton.textContent = 'Show Details';
   this.detailsButton.addEventListener('click', function(){
-    if (details.open === false){
+    if (! details.open){
       details.render();
       details.refreshButton();
     } else {
@@ -30,15 +28,9 @@ details.refreshButton = function(){
   details.buttonEl = document.createElement('img');
   details.buttonEl.setAttribute('src', 'images/refresh.jpg');
   details.buttonEl.className = 'refreshImg';
-  details.buttonEl.addEventListener('click', function(){
-    details.render();
-  })
-  details.buttonEl.addEventListener('mouseover', function(){
-    details.buttonEl.setAttribute('src', 'images/refreshO.jpg');
-  })
-  details.buttonEl.addEventListener('mouseout', function(){
-    details.buttonEl.setAttribute('src', 'images/refresh.jpg');
-  })
+  details.buttonEl.addEventListener('click', function(){details.render();})
+  details.buttonEl.addEventListener('mouseover', function(){details.buttonEl.setAttribute('src', 'images/refreshO.jpg');})
+  details.buttonEl.addEventListener('mouseout', function(){details.buttonEl.setAttribute('src', 'images/refresh.jpg');})
   details.div.appendChild(details.buttonEl);
 }
 details.render = function(){
@@ -51,18 +43,13 @@ details.render = function(){
     chartData.labels.push(product.itemName);
     chartData.datasets[0].data.push(product.clickCount);
     chartData.datasets[1].data.push(product.renderCount);
-  }, this);
+  });
   this.details = document.getElementById('myChart').getContext("2d");
-  if(this.fresh === false){
-    console.log('refreshing chart..')
-    this.detChart.destroy();
-  }
+  if(! this.fresh){this.detChart.destroy()};
   this.detChart = new Chart(this.details).Bar(chartData, {responsive: true});
   this.fresh = false;
 };
-
-function item(indexNum, name, filepath){
-  this.indexNum = indexNum;
+function item(name, filepath){
   this.itemName = name;
   this.filepath = filepath;
   this.renderCount = 0;
@@ -73,29 +60,20 @@ item.prototype.render = function() {
   var container = document.getElementById('container');
   this.divEl = document.createElement('div');
   this.divEl.className = 'imgContainer';
-
   this.divEl.addEventListener('click', function(){
     objRef.clickCount += 1;
     details.numChosen += 1;
-    console.log('The user selected ' + objRef.itemName + ' ' + objRef.clickCount + ' time(s).');
     console.log('The user has made ' + details.numChosen + ' selections.')
     refresh();
     if (details.numChosen === 15) {details.showDetails();}
   })
-
   container.appendChild(this.divEl);
   this.img = document.createElement('img');
   this.img.setAttribute('src', this.filepath);
   this.img.setAttribute('style', 'opacity:0.6');
   this.img.className = 'itemImg';
-
-  this.img.addEventListener('mouseover', function(){
-    objRef.superSizeMe(260, 280, 2, 8, 0.6, 1);
-  })
-  this.img.addEventListener('mouseout', function(){
-    objRef.superSizeMe(280, 260, -2, 8, 1, 0.6);
-  })
-
+  this.img.addEventListener('mouseover', function(){objRef.superSizeMe(260, 280, 2, 8, 0.6, 1);})
+  this.img.addEventListener('mouseout', function(){objRef.superSizeMe(280, 260, -2, 8, 1, 0.6);})
   this.divEl.appendChild(this.img);
   this.text = document.createElement('h3');
   this.text.textContent = this.itemName;
@@ -103,7 +81,6 @@ item.prototype.render = function() {
   this.divEl.appendChild(this.text);
   this.renderCount += 1;
 }
-
 item.prototype.superSizeMe = function(start, target, rate, delayRate, opacityStart, opacityTarget) {
   var delay = 0;
   var size = start;
@@ -126,17 +103,16 @@ item.prototype.resize = function(size, objRef, delay, opacity){
     obj.text.setAttribute('style', 'opacity:' + opacity);
   }, delay);
 }
-
 var chartData = {
   labels: [],
   datasets: [
     {
       label: "Number of Clicks",
       data: [],
-      fillColor: "rgba(220,220,220,0.5)",
-      strokeColor: "rgba(220,220,220,0.8)",
-      highlightFill: "rgba(220,220,220,0.75)",
-      highlightStroke: "rgba(220,220,220,1)",
+      fillColor: "rgba(199,255,171,0.8)",
+      strokeColor: "rgba(90,196,38,0.5)",
+      highlightFill: "rgba(199,255,171,1)",
+      highlightStroke: "rgba(90,196,38,0.75)"
     },
     {
       label: "Click/Render Ratio",
@@ -148,45 +124,23 @@ var chartData = {
     }
   ]
 };
-
 function randNums(){ // Returns an array of 3 unique random numbers between 0 and 14
   var array = [0, 0, 0];
   while (array[0] === array[1] || array[1] === array[2] || array[0] === array[2]){
-    array[0] = Math.floor(Math.random() * 14);
-    array[1] = Math.floor(Math.random() * 14);
-    array[2] = Math.floor(Math.random() * 14);
+    array.forEach(function(zero, index){array[index] = Math.floor(Math.random() * 14)});
   }
   return array;
 }
-
 function printItems() {
   details.chosen = randNums();
-  details.items[details.chosen[0]].render();
-  details.items[details.chosen[1]].render();
-  details.items[details.chosen[2]].render();
+  details.chosen.forEach(function(itemNum){details.items[itemNum].render()});
 }
-
 function refresh() {
   var container = document.getElementById('container');
-  container.removeChild(details.items[details.chosen[0]].divEl);
-  container.removeChild(details.items[details.chosen[1]].divEl);
-  container.removeChild(details.items[details.chosen[2]].divEl);
+  details.chosen.forEach(function(itemNum){container.removeChild(details.items[itemNum].divEl)});
   printItems();
 }
 
-details.items[0] = new item(0, 'R2D2 Bag', 'images/bag.jpg');
-details.items[1] = new item(1, 'Banana Cutter', 'images/banana.jpg');
-details.items[2] = new item(2, 'Open-Toe Boots', 'images/boots.jpg');
-details.items[3] = new item(3, 'Uncomfortable Chair', 'images/chair.jpg');
-details.items[4] = new item(4, 'Cthulhu', 'images/cthulhu.jpg');
-details.items[5] = new item(5, 'Dragon Meat', 'images/dragon.jpg');
-details.items[6] = new item(6, 'Pen Utensils', 'images/pen.jpg');
-details.items[7] = new item(7, 'Pizza Scissors', 'images/scissors.jpg');
-details.items[8] = new item(8, 'Tauntaun Sleeping Bag', 'images/sleepingbag.jpg');
-details.items[9] = new item(9, 'Baby Sweeper', 'images/sweep.png');
-details.items[10] = new item(10, 'Unicorn Meat', 'images/unicorn.jpg');
-details.items[11] = new item(11, 'USB Tentacle', 'images/usb.gif');
-details.items[12] = new item(12, 'Water Can', 'images/water-can.jpg');
-details.items[13] = new item(13, 'Wine Glass', 'images/wine-glass.jpg');
-
+var itemArgs = [['R2D2 Bag', 'images/bag.jpg'], ['Banana Cutter', 'images/banana.jpg'], ['Open-Toe Boots', 'images/boots.jpg'], ['Uncomfortable Chair', 'images/chair.jpg'], ['Cthulhu', 'images/cthulhu.jpg'], ['Dragon Meat', 'images/dragon.jpg'], ['Pen Utensils', 'images/pen.jpg'], ['Pizza Scissors', 'images/scissors.jpg'], ['Tauntaun Sleeping Bag', 'images/sleepingbag.jpg'], ['Baby Sweeper', 'images/sweep.png'], ['Unicorn Meat', 'images/unicorn.jpg'], ['USB Tentacle', 'images/usb.gif'], ['Water Can', 'images/water-can.jpg'], ['Wine Glass', 'images/wine-glass.jpg']];
+itemArgs.forEach(function(args, index){details.items[index] = new item(args[0], args[1])});
 printItems();
